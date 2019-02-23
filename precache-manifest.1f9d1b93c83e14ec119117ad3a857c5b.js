90fe2dd7d44f5e46c187bf3ea0634e63f7166a76
maieulChevalier <maieul.chevalier@gmail.com>
2019-02-23 21:36:19 +0800

    Updates

---
+++ precache-manifest.1f9d1b93c83e14ec119117ad3a857c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8694c7adb44b254e1cc7",
+    "url": "/robofriends/static/css/main.1c2e8a9c.chunk.css"
+  },
+  {
+    "revision": "8694c7adb44b254e1cc7",
+    "url": "/robofriends/static/js/main.8694c7ad.chunk.js"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "8dc83688de51eb26cb93",
+    "url": "/robofriends/static/css/2.453632e1.chunk.css"
+  },
+  {
+    "revision": "8dc83688de51eb26cb93",
+    "url": "/robofriends/static/js/2.8dc83688.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8067db892127a73874518b44eaa6df5b",
+    "url": "/robofriends/index.html"
+  }
+];
